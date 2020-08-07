9ffd22a7b9b1dbe6927f0addafe488595eaa8899
Margaret Matocha <ksmatocha@gmail.com>
2020-08-06 19:37:41 -0500

    Ran black on examples

--- examples/display_text_label_vs_bitmap_label_alternating.py
+++ examples/display_text_label_vs_bitmap_label_alternating.py
@@ -5,8 +5,8 @@
 # Moves the position of the textBox around the display
 # Clears each textBox after the full string is written (even if the text is outside of the box)
 
-#import textmap
-#from textmap import textBox
+# import textmap
+# from textmap import textBox
 
 import board
 import displayio
@@ -17,7 +17,8 @@ import time
 import busio
 
 from adafruit_display_text import bitmap_label
-#from adafruit_display_text import bitmap_label as Label
+
+# from adafruit_display_text import bitmap_label as Label
 
 from adafruit_display_text import label
 
@@ -25,12 +26,12 @@ from adafruit_display_text import label
 ##########
 # Use this Boolean variables to select which font style to use
 ##########
-use_builtin_font=True   # Set True to use the terminalio.FONT BuiltinFont, 
-                        # Set False to use a BDF loaded font, see "fontFiles" below
+use_builtin_font = True  # Set True to use the terminalio.FONT BuiltinFont,
+# Set False to use a BDF loaded font, see "fontFiles" below
 ##########
 
 # Set scaling factor for display text
-my_scale=1
+my_scale = 1
 
 #  Setup the SPI display
 
@@ -83,145 +84,168 @@ else:
     # built-in display
     display = board.DISPLAY
 
-print ('Display is started')
+print("Display is started")
 
 
 # load all the fonts
-print('loading fonts...')
+print("loading fonts...")
 
 fontList = []
 
 # Load some proportional fonts
-fontFiles =   [
-            'fonts/Helvetica-Bold-16.bdf',
-#            'fonts/BitstreamVeraSans-Roman-24.bdf', # Header2
-#            'fonts/BitstreamVeraSans-Roman-16.bdf', # mainText
-            ]
+fontFiles = [
+    "fonts/Helvetica-Bold-16.bdf",
+    #            'fonts/BitstreamVeraSans-Roman-24.bdf', # Header2
+    #            'fonts/BitstreamVeraSans-Roman-16.bdf', # mainText
+]
 
 from adafruit_bitmap_font import bitmap_font
 
 for i, fontFile in enumerate(fontFiles):
 
-    if use_builtin_font: 
-        thisFont=terminalio.FONT  # comment this out to switch back to BDF loaded fonts
+    if use_builtin_font:
+        thisFont = (
+            terminalio.FONT
+        )  # comment this out to switch back to BDF loaded fonts
     else:
-        thisFont = bitmap_font.load_font(fontFile) 
+        thisFont = bitmap_font.load_font(fontFile)
 
     fontList.append(thisFont)
 
 
-preloadTheGlyphs= True # set this to True if you want to preload the font glyphs into memory
-    # preloading the glyphs will help speed up the rendering of text but will use more RAM
+preloadTheGlyphs = (
+    True  # set this to True if you want to preload the font glyphs into memory
+)
+# preloading the glyphs will help speed up the rendering of text but will use more RAM
 
 if preloadTheGlyphs:
 
     # identify the glyphs to load into memory -> increases rendering speed
-    glyphs = b'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-,.:?! '
+    glyphs = b"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-,.:?! "
 
-    print('loading glyphs...')
+    print("loading glyphs...")
     for font in fontList:
         if font is not terminalio.FONT:
             font.load_glyphs(glyphs)
 
-    print('Glyphs are loaded.')
+    print("Glyphs are loaded.")
 
 
-print('Fonts completed loading.')
+print("Fonts completed loading.")
 
-# create group 
+# create group
 import gc
 
 
-myString1=('This is a label.py and\nbitmap_label.py comparison.')
-myString23='none' 
-myString_bitmap_label='bitmap_label'
-myString_label='label                               bitmap_label'
+myString1 = "This is a label.py and\nbitmap_label.py comparison."
+myString23 = "none"
+myString_bitmap_label = "bitmap_label"
+myString_label = "label                               bitmap_label"
 
 
 #####
 # Create the "bitmap_label.py" versions of the text labels.
 
 gc.collect()
-bitmap_label_start=gc.mem_free()
+bitmap_label_start = gc.mem_free()
 
-bmap_label1 = bitmap_label.Label(font=fontList[0], text=myString1, color=0xFFFFFF, max_glyphs=len(myString1),
-                                background_color=0xFF0000,
-                                padding_bottom=0,
-                                padding_left=0,
-                                padding_right=0,
-                                padding_top=0,
-                                background_tight=True,
-                                x=10,
-                                y=60,
-                                line_spacing=1.25,
-                                scale=my_scale,
-                                anchor_point=(0.5,0),
-                                anchored_position=(160, 50),
-                                )
-label2_padding=10
-bmap_label2 = bitmap_label.Label(font=fontList[0], text=myString23, color=0x000000, max_glyphs=len(myString23),
-                                background_color=0xFFFF00,
-                                padding_bottom=label2_padding,
-                                padding_left=0,
-                                padding_right=0,
-                                padding_top=label2_padding,
-                                background_tight=False,
-                                x=10,
-                                y=100,
-                                line_spacing=1.25,
-                                scale=my_scale,
-                                anchor_point=(0,0),
-                                anchored_position=(200,150),
-                                )
+bmap_label1 = bitmap_label.Label(
+    font=fontList[0],
+    text=myString1,
+    color=0xFFFFFF,
+    max_glyphs=len(myString1),
+    background_color=0xFF0000,
+    padding_bottom=0,
+    padding_left=0,
+    padding_right=0,
+    padding_top=0,
+    background_tight=True,
+    x=10,
+    y=60,
+    line_spacing=1.25,
+    scale=my_scale,
+    anchor_point=(0.5, 0),
+    anchored_position=(160, 50),
+)
+label2_padding = 10
+bmap_label2 = bitmap_label.Label(
+    font=fontList[0],
+    text=myString23,
+    color=0x000000,
+    max_glyphs=len(myString23),
+    background_color=0xFFFF00,
+    padding_bottom=label2_padding,
+    padding_left=0,
+    padding_right=0,
+    padding_top=label2_padding,
+    background_tight=False,
+    x=10,
+    y=100,
+    line_spacing=1.25,
+    scale=my_scale,
+    anchor_point=(0, 0),
+    anchored_position=(200, 150),
+)
 
-bmap_label3 = bitmap_label.Label(font=fontList[0], text=myString23, color=0x000000, max_glyphs=len(myString23),
-                                background_color=0xFFFF00,
-                                padding_bottom=0,
-                                padding_left=0,
-                                padding_right=0,
-                                padding_top=0,
-                                background_tight=True,
-                                x=10,
-                                y=150,
-                                line_spacing=1.25,
-                                scale=my_scale,
-                                )
+bmap_label3 = bitmap_label.Label(
+    font=fontList[0],
+    text=myString23,
+    color=0x000000,
+    max_glyphs=len(myString23),
+    background_color=0xFFFF00,
+    padding_bottom=0,
+    padding_left=0,
+    padding_right=0,
+    padding_top=0,
+    background_tight=True,
+    x=10,
+    y=150,
+    line_spacing=1.25,
+    scale=my_scale,
+)
 
-bmap_label4 = bitmap_label.Label(font=fontList[0], text=myString_label, color=0x000000, max_glyphs=len(myString_label),
-                                background_color=0xFFFF00,
-                                padding_bottom=0,
-                                padding_left=0,
-                                padding_right=0,
-                                padding_top=0,
-                                background_tight=False,
-                                x=10,
-                                y=200,
-                                line_spacing=1.25,
-                                scale=my_scale,
-                                )
+bmap_label4 = bitmap_label.Label(
+    font=fontList[0],
+    text=myString_label,
+    color=0x000000,
+    max_glyphs=len(myString_label),
+    background_color=0xFFFF00,
+    padding_bottom=0,
+    padding_left=0,
+    padding_right=0,
+    padding_top=0,
+    background_tight=False,
+    x=10,
+    y=200,
+    line_spacing=1.25,
+    scale=my_scale,
+)
 
-myString5='bitmap_label -->'
-bmap_label5 = bitmap_label.Label(font=fontList[0], text=myString5, color=0xFFFFFF, max_glyphs=len(myString5),
-                                background_color=0x000000,
-                                padding_bottom=0,
-                                padding_left=0,
-                                padding_right=0,
-                                padding_top=0,
-                                background_tight=True,
-                                x=10,
-                                y=200,
-                                line_spacing=1.25,
-                                anchor_point=(1,0.5),
-                                anchored_position=(200,200),
-                                scale=my_scale,
-                                )
-
+myString5 = "bitmap_label -->"
+bmap_label5 = bitmap_label.Label(
+    font=fontList[0],
+    text=myString5,
+    color=0xFFFFFF,
+    max_glyphs=len(myString5),
+    background_color=0x000000,
+    padding_bottom=0,
+    padding_left=0,
+    padding_right=0,
+    padding_top=0,
+    background_tight=True,
+    x=10,
+    y=200,
+    line_spacing=1.25,
+    anchor_point=(1, 0.5),
+    anchored_position=(200, 200),
+    scale=my_scale,
+)
 
 
 gc.collect()
-bitmap_label_end=gc.mem_free()
+bitmap_label_end = gc.mem_free()
 
-bmap_group = displayio.Group( max_size=5 ) # Create a group for displaying
+bmap_group = displayio.Group(max_size=5)  # Create a group for displaying
 bmap_group.append(bmap_label1)
 bmap_group.append(bmap_label2)
 bmap_group.append(bmap_label3)
@@ -229,90 +253,109 @@ bmap_group.append(bmap_label4)
 bmap_group.append(bmap_label5)
 
 
-
 #####
 # Create the "label.py" versions of the text labels.
 
 gc.collect()
-label_start=gc.mem_free()
+label_start = gc.mem_free()
 
-label1 = label.Label(font=fontList[0], text=myString1, color=0xFFFFFF, max_glyphs=len(myString1),
-                                background_color=0xFF0000,
-                                padding_bottom=0,
-                                padding_left=0,
-                                padding_right=0,
-                                padding_top=0,
-                                background_tight=True,
-                                x=10,
-                                y=60,
-                                line_spacing=1.25,
-                                scale=my_scale,
-                                anchor_point=(0.5,0),
-                                anchored_position=(160, 50),
-                                )
+label1 = label.Label(
+    font=fontList[0],
+    text=myString1,
+    color=0xFFFFFF,
+    max_glyphs=len(myString1),
+    background_color=0xFF0000,
+    padding_bottom=0,
+    padding_left=0,
+    padding_right=0,
+    padding_top=0,
+    background_tight=True,
+    x=10,
+    y=60,
+    line_spacing=1.25,
+    scale=my_scale,
+    anchor_point=(0.5, 0),
+    anchored_position=(160, 50),
+)
 
-label2 = label.Label(font=fontList[0], text=myString23, color=0x000000, max_glyphs=len(myString23),
-                                background_color=0xFFFF00,
-                                padding_bottom=label2_padding,
-                                padding_left=0,
-                                padding_right=0,
-                                padding_top=label2_padding,
-                                background_tight=False,
-                                x=10,
-                                y=100,
-                                line_spacing=1.25,
-                                scale=my_scale,
-                                anchor_point=(0,0),
-                                anchored_position=(200,150),
-                                )
+label2 = label.Label(
+    font=fontList[0],
+    text=myString23,
+    color=0x000000,
+    max_glyphs=len(myString23),
+    background_color=0xFFFF00,
+    padding_bottom=label2_padding,
+    padding_left=0,
+    padding_right=0,
+    padding_top=label2_padding,
+    background_tight=False,
+    x=10,
+    y=100,
+    line_spacing=1.25,
+    scale=my_scale,
+    anchor_point=(0, 0),
+    anchored_position=(200, 150),
+)
 
-label3 = label.Label(font=fontList[0], text=myString23, color=0x000000, max_glyphs=len(myString23),
-                                background_color=0xFFFF00,
-                                padding_bottom=0,
-                                padding_left=0,
-                                padding_right=0,
-                                padding_top=0,
-                                background_tight=True,
-                                x=10,
-                                y=150,
-                                line_spacing=1.25,
-                                scale=my_scale,
-                                )
+label3 = label.Label(
+    font=fontList[0],
+    text=myString23,
+    color=0x000000,
+    max_glyphs=len(myString23),
+    background_color=0xFFFF00,
+    padding_bottom=0,
+    padding_left=0,
+    padding_right=0,
+    padding_top=0,
+    background_tight=True,
+    x=10,
+    y=150,
+    line_spacing=1.25,
+    scale=my_scale,
+)
 
-label4 = label.Label(font=fontList[0], text=myString_label, color=0x000000, max_glyphs=len(myString_label),
-                                background_color=0xFFFF00,
-                                padding_bottom=0,
-                                padding_left=0,
-                                padding_right=0,
-                                padding_top=0,
-                                background_tight=True,
-                                x=10,
-                                y=200,
-                                line_spacing=1.25,
-                                scale=my_scale,
-                                )
+label4 = label.Label(
+    font=fontList[0],
+    text=myString_label,
+    color=0x000000,
+    max_glyphs=len(myString_label),
+    background_color=0xFFFF00,
+    padding_bottom=0,
+    padding_left=0,
+    padding_right=0,
+    padding_top=0,
+    background_tight=True,
+    x=10,
+    y=200,
+    line_spacing=1.25,
+    scale=my_scale,
+)
 
 
-myString5 = '<-- label'
-label5 = label.Label(font=fontList[0], text=myString5, color=0xFFFFFF, max_glyphs=len(myString5),
-                                background_color=0x000000,
-                                padding_bottom=0,
-                                padding_left=0,
-                                padding_right=0,
-                                padding_top=0,
-                                background_tight=False,
-                                x=10,
-                                y=200,
-                                line_spacing=1.25,
-                                anchor_point=(0,0.5),
-                                anchored_position=(50,200),
-                                scale=my_scale,
-                                )
+myString5 = "<-- label"
+label5 = label.Label(
+    font=fontList[0],
+    text=myString5,
+    color=0xFFFFFF,
+    max_glyphs=len(myString5),
+    background_color=0x000000,
+    padding_bottom=0,
+    padding_left=0,
+    padding_right=0,
+    padding_top=0,
+    background_tight=False,
+    x=10,
+    y=200,
+    line_spacing=1.25,
+    anchor_point=(0, 0.5),
+    anchored_position=(50, 200),
+    scale=my_scale,
+)
 
 gc.collect()
-label_end=gc.mem_free()
+label_end = gc.mem_free()
 
-label_group = displayio.Group( max_size=5 ) # Create a group for displaying
+label_group = displayio.Group(max_size=5)  # Create a group for displaying
 label_group.append(label1)
 label_group.append(label2)
 label_group.append(label3)
@@ -320,26 +363,18 @@ label_group.append(label4)
 label_group.append(label5)
 
 
+print("***")
 
-
-print('***')
-
-display.auto_refresh=True
+display.auto_refresh = True
 
 while True:
-    print('bitmap_label')
+    print("bitmap_label")
     time.sleep(0.1)
     display.show(bmap_group)
-    
+
     time.sleep(2)
 
-    print('label')
+    print("label")
     time.sleep(0.1)
     display.show(label_group)
     time.sleep(2)
-
-
-
-
-
-
--- examples/display_text_label_vs_bitmap_label_simpletest.py
+++ examples/display_text_label_vs_bitmap_label_simpletest.py
@@ -5,8 +5,8 @@
 # Moves the position of the textBox around the display
 # Clears each textBox after the full string is written (even if the text is outside of the box)
 
-#import textmap
-#from textmap import textBox
+# import textmap
+# from textmap import textBox
 
 import board
 import displayio
@@ -17,25 +17,25 @@ import time
 ##########
 # Use these Boolean variables to select the text display library and which font style to use
 ##########
-use_bitmap_label=True  # Set True if to use 'bitmap_label.py'
-                        # Set False to use 'label.py' library
+use_bitmap_label = True  # Set True if to use 'bitmap_label.py'
+# Set False to use 'label.py' library
 ##########
-use_builtin_font=True   # Set True to use the terminalio.FONT BuiltinFont, 
-                        # Set False to use a BDF loaded font, see "fontFiles" below
+use_builtin_font = True  # Set True to use the terminalio.FONT BuiltinFont,
+# Set False to use a BDF loaded font, see "fontFiles" below
 ##########
 
 
-my_scale=1
+my_scale = 1
 
-if use_bitmap_label: # use bitmap_label.py library (Bitmap)
+if use_bitmap_label:  # use bitmap_label.py library (Bitmap)
     from adafruit_display_text import bitmap_label as label
 
-    version='bitmap_label.py'
+    version = "bitmap_label.py"
 
-else: # use label.py library (TileGrid)
+else:  # use label.py library (TileGrid)
     from adafruit_display_text import label as label
 
-    version='label.py'
+    version = "label.py"
 
 #  Setup the SPI display
 
@@ -88,95 +88,99 @@ else:
     # built-in display
     display = board.DISPLAY
 
-print ('Display is started')
+print("Display is started")
 
 # load all the fonts
-print('loading fonts...')
-
-
+print("loading fonts...")
 
 
 # Setup file locations for BDF font files
-font_files =   [
-            'fonts/Helvetica-Bold-16.bdf',
-#            'fonts/BitstreamVeraSans-Roman-24.bdf', 
-#            'fonts/BitstreamVeraSans-Roman-16.bdf', 
-#            'fonts/Fayette-HandwrittenScript-64.bdf',
-            ]
+font_files = [
+    "fonts/Helvetica-Bold-16.bdf",
+    #            'fonts/BitstreamVeraSans-Roman-24.bdf',
+    #            'fonts/BitstreamVeraSans-Roman-16.bdf',
+    #            'fonts/Fayette-HandwrittenScript-64.bdf',
+]
 
 font_list = []
 
 for i, font_file in enumerate(font_files):
-    
+
     if use_builtin_font:
-        this_font=terminalio.FONT  # comment this out to switch back to BDF loaded fonts
+        this_font = (
+            terminalio.FONT
+        )  # comment this out to switch back to BDF loaded fonts
     else:
         from adafruit_bitmap_font import bitmap_font
-        this_font = bitmap_font.load_font(font_file) 
+
+        this_font = bitmap_font.load_font(font_file)
 
     font_list.append(this_font)
 
 
-preload_the_glyphs= True # set this to True if you want to preload the font glyphs into memory
-    # preloading the glyphs will help speed up the rendering of text but will use more RAM
+preload_the_glyphs = (
+    True  # set this to True if you want to preload the font glyphs into memory
+)
+# preloading the glyphs will help speed up the rendering of text but will use more RAM
 
 if preload_the_glyphs:
 
     # identify the glyphs to load into memory -> increases rendering speed
-    glyphs = b'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-,.:?! '
+    glyphs = b"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-,.:?! "
 
-    print('loading glyphs...')
+    print("loading glyphs...")
     for font in font_list:
         if font is not terminalio.FONT:
             font.load_glyphs(glyphs)
 
-    print('Glyphs are loaded.')
+    print("Glyphs are loaded.")
 
-print('Fonts completed loading.')
+print("Fonts completed loading.")
 
-# create group 
+# create group
 import gc
-gc.collect()
-print( 'After creating Group,  Memory free: {}'.format(gc.mem_free()) )
-
-
-
-my_string=('Welcome to using displayio on CircuitPython!')
 
 gc.collect()
-label_start_memory=gc.mem_free()
-start_time=time.monotonic()
+print("After creating Group,  Memory free: {}".format(gc.mem_free()))
 
-bmap_label = label.Label(font=font_list[0], text=my_string, color=0xFFFFFF, max_glyphs=len(my_string),
-                                background_color=0xFF0000,
-                                padding_bottom=0,
-                                padding_left=0,
-                                padding_right=0,
-                                padding_top=0,
-                                background_tight=False,
-                                x=10,
-                                y=30,
-                                line_spacing=1.25,
-                                scale=my_scale,
-                                )
-end_time=time.monotonic()
+
+my_string = "Welcome to using displayio on CircuitPython!"
 
 gc.collect()
-label_end_memory=gc.mem_free()
+label_start_memory = gc.mem_free()
+start_time = time.monotonic()
 
-bmap_group = displayio.Group( max_size=1 ) # Create a group for displaying
+bmap_label = label.Label(
+    font=font_list[0],
+    text=my_string,
+    color=0xFFFFFF,
+    max_glyphs=len(my_string),
+    background_color=0xFF0000,
+    padding_bottom=0,
+    padding_left=0,
+    padding_right=0,
+    padding_top=0,
+    background_tight=False,
+    x=10,
+    y=30,
+    line_spacing=1.25,
+    scale=my_scale,
+)
+end_time = time.monotonic()
+
+gc.collect()
+label_end_memory = gc.mem_free()
+
+bmap_group = displayio.Group(max_size=1)  # Create a group for displaying
 bmap_group.append(bmap_label)
 
 
-print('***')
-print('{} memory used: {}'.format(version, label_start_memory-label_end_memory))
-print('{} time to process: {} seconds'.format(version, end_time-start_time))
-display.auto_refresh=True
+print("***")
+print("{} memory used: {}".format(version, label_start_memory - label_end_memory))
+print("{} time to process: {} seconds".format(version, end_time - start_time))
+display.auto_refresh = True
 
 display.show(bmap_group)
 
 while True:
     pass
-
-
-
