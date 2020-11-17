006fee4a6cdb1e00aac9fe04fe87785504649858
foamyguy <foamyguy@gmail.com>
2020-11-16 22:03:05 -0600

    black format

--- examples/display_text_wraptest.py
+++ examples/display_text_wraptest.py
@@ -11,10 +11,12 @@ from adafruit_display_text import label, wrap_text_to_lines
 # https://learn.adafruit.com/circuitpython-display-support-using-displayio/display-and-display-bus
 display = board.DISPLAY
 
-text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " \
-       "sed do eiusmod tempor incididunt ut labore et dolore magna " \
-       "aliqua. Ut enim ad minim veniam, quis nostrud exercitation " \
-       "ullamco laboris nisi ut aliquip ex ea commodo consequat."
+text = (
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, "
+    "sed do eiusmod tempor incididunt ut labore et dolore magna "
+    "aliqua. Ut enim ad minim veniam, quis nostrud exercitation "
+    "ullamco laboris nisi ut aliquip ex ea commodo consequat."
+)
 text = "\n".join(wrap_text_to_lines(text, 28))
 text_area = label.Label(terminalio.FONT, text=text)
 text_area.x = 10
