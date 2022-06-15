367af1de0990254c06263f37c8c2e8139c6453de
Alec Delaney <tekktrik@gmail.com>
2022-06-14 23:46:51 -0400

    Add __version__ and __repo__

--- adafruit_display_text/__init__.py
+++ adafruit_display_text/__init__.py
@@ -7,6 +7,9 @@
 =======================
 """
 
+__version__ = "0.0.0-auto.0"
+__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_Display_Text.git"
+
 from displayio import Group, Palette
 
 try:
